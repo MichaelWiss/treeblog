2d4fab91dae88e870618812349fc3c4f14906d4f
Michael Wiss <michael.wiss@gmail.com>
2014-07-23 13:53:19 -0400

    filled out articles_controller

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -1,11 +1,24 @@
 class ArticlesController < ApplicationController
 
+def new
+   @article = Article.new
+end
+
 def index
 	@articles = Article.all
 end
 
 def show
     @article = Article.find(params[:id])
+end
+
+def create
+  @article = Article.new
+  @article.title = params[:article][:title]
+  @article.body = params[:article][:body]
+  @article.save
+  redirect_to article_path(@article)
+
 end
 
 
