555488e54178bc49e641e187bd5ae040aaf7a8e3
Michael Wiss <michael.wiss@gmail.com>
2014-07-24 12:49:39 -0400

    finished blog comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,12 +8,14 @@ end
 
 def index
 	@articles = Article.all
-  
+
 
 end
 
 def show
     @article = Article.find(params[:id])
+    @comment = Comment.new
+    @comment.article_id = @article.id
 end
 
  def create
