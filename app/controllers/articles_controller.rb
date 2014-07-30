7afaca29023f8c65ec2d1f3a01781e7dedde125d
Michael Wiss <michael.wiss@gmail.com>
2014-07-30 18:06:32 -0400

    fixed articles_controller?

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,26 +4,19 @@ include ArticlesHelper
 
 def new
    @article = Article.new
-
 end
 
 def index
-
 	@articles = Article.all
   @tag = Tag.all
-  
 
 
 end
 
-
-
 def show
-
     @article = Article.find(params[:id])
     @comment = Comment.new
     @comment.article_id = @article.id
-
 
 end
 
@@ -36,7 +29,7 @@ end
 
    def create
         @article = Article.new(article_params)
-
+       
         @article.save
         flash.notice = "Article '#{@article.title}' Posted!"
         redirect_to article_path(@article)
@@ -65,6 +58,5 @@ end
   def user_params
   params.require(:user).permit(:image)
 end
-
 
 end
