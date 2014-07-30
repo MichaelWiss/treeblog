d6ead0565e28d72f41a5c2543aceed27524fc46d
Michael Wiss <michael.wiss@gmail.com>
2014-07-30 12:58:16 -0400

    added end to article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,7 +8,7 @@ class Article < ActiveRecord::Base
 
 	has_attached_file :image
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-  
+  end
 
    def tag_list
        tags.join(", ")
