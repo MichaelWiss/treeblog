d9cc5770a58083ee4a474f0a1172394e875afdee
Michael Wiss <michael.wiss@gmail.com>
2014-07-30 11:13:09 -0400

    push to heroku

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,3 @@
-Paperclip::Attachment.default_options[:url] = ':treeblog.s3.amazonaws.com'
+Paperclip::Attachment.default_options[:url] = ':treeblog.s3-website-us-west-2.amazonaws.com'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+Paperclip::Attachment.default_options[:s3_host_name] = 'treeblog.s3-website-us-west-2.amazonaws.com'
