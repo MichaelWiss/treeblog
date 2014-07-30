9c1125bbc939d36b8e31e8e127cbbda052f12d60
Michael Wiss <michael.wiss@gmail.com>
2014-07-30 12:47:33 -0400

    chnged validation in article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,8 +7,8 @@ class Article < ActiveRecord::Base
 	has_many :featured
 
 	has_attached_file :image
-	
-    validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  
 
    def tag_list
        tags.join(", ")
